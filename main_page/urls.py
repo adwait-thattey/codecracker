0cc7e3fb020ec1f9d4d099e6b31d02ea4564a864
coder-dude <coderdude1999@gmail.com>
2018-10-04 01:06:28 +0530

    modified urls

--- main_page/urls.py
+++ main_page/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.landing_page, name='landing'),
-    # TODO Change these both to switch between landing and home based on user login status later if required
+    path('index', views.landing_page, name='landing'),
     path('home', views.home, name='home'),
+    path('', views.redirect_to_landing)
+    # TODO Change this to switch between landing and home based on user login status later if required
+
 ]
