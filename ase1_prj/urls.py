0d2e64673ce08c98d453833a5456a0f9f53360a7
coder-dude <coderdude1999@gmail.com>
2018-10-03 23:58:28 +0530

    setup urls

--- ase1_prj/urls.py
+++ ase1_prj/urls.py
@@ -14,8 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
+
+
+from . import settings
 
 urlpatterns = [
+
+    path('', include('main_page.urls')),
     path('admin/', admin.site.urls),
+    # path('registration/', include('registration.urls')),
+
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
