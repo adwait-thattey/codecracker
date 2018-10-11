868903c5a9a08825f8301e91aaed2cb22cbe537a
BHARATHS0101 <bharath.s17@iiits.in>
2018-10-11 15:43:11 +0530

    s

--- main_page/urls.py
+++ main_page/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('index', views.landing_page, name='landing'),
     path('home', views.home, name='home'),
+    path('nav', views.nav, name='nav'),
     path('', views.redirect_to_landing)
     # TODO Change this to switch between landing and home based on user login status later if required
 
