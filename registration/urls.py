45e8150a980d15b430fed45bf4ba477296382ab7
NikhilMunna <nikhil.munna001@gmail.com>
2018-11-01 23:48:52 +0530

    updated registrations.urls

--- registration/urls.py
+++ registration/urls.py
@@ -6,4 +6,9 @@ from . import views
 urlpatterns = [
     path('login', LoginView.as_view(template_name="registration/login.html"), name="login"),
     path('register', views.signup, name= 'signup'),
+    path('password_reset/', auth_views.password_reset, name='password_reset'),
+    path('password_reset/done/', auth_views.password_reset_done, name='password_reset_done'),
+    path('reset/<uidb64>/<token>', auth_views.password_reset_confirm, name='password_reset_confirm'),
+    path('reset/done/', auth_views.password_reset_complete, name='password_reset_complete'),
+
     ]
