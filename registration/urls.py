777173bf5460866d27f8c5870b9137a40df5bd32
BHARATHS0101 <bharath.s17@iiits.in>
2018-11-04 14:53:41 +0530

    added email confirmation

--- registration/urls.py
+++ registration/urls.py
@@ -5,4 +5,5 @@ app_name = "registration"
 urlpatterns = [
     path('login', auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
     path('register', views.signup, name='signup'),
+    path('activate/<uidb64>/<token>', views.activate, name='activate'),
 ]
