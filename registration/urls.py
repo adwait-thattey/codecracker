061c324955b517910aa0d8ec1462e3c76bbeac44
coder-dude <coderdude1999@gmail.com>
2018-11-13 22:48:32 +0530

    fix wrongly handled merge conflict

--- registration/urls.py
+++ registration/urls.py
@@ -1,26 +1,12 @@
 from django.urls import path
-<<<<<<< Updated upstream
-from django.contrib.auth import views as auth_views
-=======
-import django.contrib.auth.views
+
 from django.contrib.auth import views as auth_views
 
->>>>>>> Stashed changes
 from . import views
 app_name = "registration"
 urlpatterns = [
-<<<<<<< Updated upstream
     path('login', auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
     path('register', views.signup, name='signup'),
     path('activate/<uidb64>/<token>', views.activate, name='activate'),
 ]
-=======
-    path('login', django.contrib.auth.views.LoginView.as_view(template_name="registration/login.html"), name="login"),
-    path('register', views.signup, name= 'signup'),
-    path('password_reset/', auth_views.password_reset, name='password_reset'),
-    path('password_reset/done/', auth_views.password_reset_done, name='password_reset_done'),
-    path('reset/<uidb64>/<token>', auth_views.password_reset_confirm, name='password_reset_confirm'),
-    path('reset/done/', auth_views.password_reset_complete, name='password_reset_complete'),
 
-    ]
->>>>>>> Stashed changes
