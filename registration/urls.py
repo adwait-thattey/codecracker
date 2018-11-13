4aff5bae8d1bbbb49e62b676d9bda29240e0d519
coder-dude <coderdude1999@gmail.com>
2018-11-13 22:46:24 +0530

    Revert "solved issue in registration/urls"
    This reverts commit 2eb687ef1b8311aab13160109266ec2a303b0559.

--- registration/urls.py
+++ registration/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from django.contrib.auth import views as auth_views
 import django.contrib.auth.views
+from django.contrib.auth import views as auth_views
 
 
 from . import views
