97e2429c73efd353393001f8453fc150ef47aaf2
coder-dude <coderdude1999@gmail.com>
2018-11-27 00:19:53 +0530

    fixed multiple objects error

--- questions/urls.py
+++ questions/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('browse/<int:category>/<int:sortby>/<int:rev>', views.browse_args_questions,name="browse_args"),
     path('<slug:question_unique_id>/submit', views.submit_solution, name="submit-solution"),
 
-    path('<slug:question_unique_id>/submission/<int:submission_attempt>/result', views.submission_result, name="submission-result"),
+    path('<slug:question_unique_id>/<str:username>/submission/<int:submission_attempt>/result', views.submission_result, name="submission-result"),
     path('ajax/submission-result', views.ajax_get_submission_results, name="ajax-submission-result"),
     path('<slug:question_unique_id>/view/', views.view_the_question, name="view_the_question"),
     path('post/', views.post_question, name="post_question"),
