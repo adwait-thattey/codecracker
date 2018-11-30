235e5234c7aab601302af4b0262ca31105522af6
coder-dude <coderdude1999@gmail.com>
2018-11-30 19:25:13 +0530

    made first statistics util function

--- ase1_prj/settings.py
+++ ase1_prj/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'ckeditor',
     'ckeditor_uploader',
     'contests',
+    'stats'
 ]
 
 CKEDITOR_UPLOAD_PATH = "ckeditor_uploads/"
---
+++ stats/views.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.models import User
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404
+from django.db.models import Count
+
+# Create your views here.
+# Anybody can get anybody's stats
+# There is no restriction
+from questions.models import Submission
+
+
+def user_submission_date_stats(request, username):
+    user = get_object_or_404(User, username=username)
+    all_submissions = user.submission_set.all()
+    # all_submissions = Submission.objects
+    submission_dates = all_submissions.values('submitted_on__date').annotate(count=Count('id')).values('submitted_on__date', 'count')
+
+    req_stats = list()
+    if submission_dates:
+
+        cur_date = submission_dates[0]['submitted_on__date']
+        cur_count = 0
+        for S in submission_dates:
+            if S['submitted_on__date']==cur_date:
+                cur_count+=S['count']
+            else:
+                D = {"date":cur_date.strftime('%d/%m/%Y'), "count":cur_count}
+                req_stats.append(D)
+                cur_date = S['submitted_on__date']
+                cur_count = S['count']
+
+
+
+    return req_stats
