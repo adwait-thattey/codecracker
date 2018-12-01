d6ebf058ff8df2896c8498a96eb0e958499a9481
coder-dude <coderdude1999@gmail.com>
2018-12-02 00:35:46 +0530

    made custom error pages

--- main_page/urls.py
+++ main_page/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from django.conf.urls import handler500
+
 from . import views
 
 
@@ -10,8 +12,10 @@ urlpatterns = [
     path('', views.redirect_to_landing),
     path('nav', views.nav, name='nav'),
     path('ajax/get_all_notifications', views.get_all_notifications ,name="all_notifications"),
-    path('ajax/get_unseen_notifications', views.get_unseen_notifications, name="unseen_notifications")
-
+    path('ajax/get_unseen_notifications', views.get_unseen_notifications, name="unseen_notifications"),
+    path('503', views.error_500, name="raise-503"),
+    path('404', views.error_404, name="raise-404")
     # TODO Change this to switch between landing and home based on user login status later if required
 
 ]
+
