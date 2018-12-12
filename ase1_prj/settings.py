315f1ac9b87aa82fa771a8628670f79e95a1108b
coder-dude <coderdude1999@gmail.com>
2018-12-12 22:50:48 +0530

    setup postgres

--- ase1_prj/settings.py
+++ ase1_prj/settings.py
@@ -191,10 +191,21 @@ WSGI_APPLICATION = 'ase1_prj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'codecracker',
+        'USER': 'codecracker',
+        'PASSWORD': 'Crack The Code',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
